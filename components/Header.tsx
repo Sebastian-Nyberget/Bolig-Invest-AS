0132245e67b72c908f6677a728f084a819247735
Sebastian-Nyberget <nybesebe@gmail.com>
2025-01-13 20:11:21 +0100

    Fix header navigation links

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,19 @@
+"use client";
+
 import { FaHouse } from "react-icons/fa6";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const pathname = usePathname();
+
+  const navItems = [
+    { name: "Hjem", path: "/" },
+    { name: "Om oss", path: "/om-oss" },
+    { name: "Prosjekter", path: "/prosjekter" },
+  ];
+
   return (
     <nav className="flex justify-between my-8 mx-24">
       <div className="w-60">
@@ -13,9 +24,20 @@ const Header = () => {
       </div>
       <div className="w-60">
         <ul className="flex space-x-4">
-          <Link href="/">Hjem</Link>
-          <Link href="/om-oss">Om oss</Link>
-          <Link href="/prosjekter">Prosjekter</Link>
+          {navItems.map((item) => (
+            <li key={item.path}>
+              <Link
+                href={item.path}
+                className={`${
+                  pathname === item.path
+                    ? "font-bold"
+                    : "hover:text-gray-400 hover:underline"
+                }`}
+              >
+                {item.name}
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
       <div className="w-60">
